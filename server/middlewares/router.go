aff45e6db74e66a4d4dbbc8f2a94b9341586b1a2
alexis-piquet <alexis.piquet.37@gmail.com>
2024-09-13 11:17:30 +0200

    Improve RouterKeeper

--- server/middlewares/router.go
+++ server/middlewares/router.go
@@ -23,7 +23,7 @@ type Route struct {
 func GetRouter(router *mux.Router, locale string, log *logger.Logger, binary_file *binary.BinaryFile) *mux.Router {
 	log.Info("MHF-API:middlewares:router:GetRouter")
 
-	initRoutes(locale)
+	checkAvailableRoutes(locale)
 
 	GetRouterEquipment(router, log, binary_file)
 	GetRouterItem(router, log, binary_file)
@@ -43,7 +43,7 @@ func RouterKeeper(log *logger.Logger, locales []string) Middleware {
 
 			if !isValidEndpoint(locales, endpoint, method) {
 				errorMessage := fmt.Sprintf("Sorry, the endpoint '%s' you are trying to access is not available at this time. Please check the URL and try again later.", endpoint)
-				http.Error(res, errorMessage, http.StatusInternalServerError)
+				http.Error(res, errorMessage, http.StatusNotImplemented)
 				log.Error(fmt.Sprintf("Invalid endpoint - Endpoint: %s | Method: %s | ClientIP: %s", endpoint, method, clientIP))
 				return
 			}
@@ -53,10 +53,19 @@ func RouterKeeper(log *logger.Logger, locales []string) Middleware {
 	}
 }
 
-func initRoutes(locale string) []Route {
-	var routes []Route
-
+func checkAvailableRoutes(locale string) {
 	fmt.Printf("Routes initialization for locale='%s'\n", locale)
+	routes := GetRoutes()
+
+	for _, route := range routes {
+		fmt.Printf("[%s] -> '%s' -> %s()\n", route.Method, locale+route.Endpoint, route.Handler)
+	}
+
+	fmt.Printf("Routes for locale='%s' initialized successfully!\n", locale)
+}
+
+func GetRoutes() []Route {
+	var routes []Route
 
 	routes = append(routes, equipment_routes...)
 	routes = append(routes, item_routes...)
@@ -64,18 +73,12 @@ func initRoutes(locale string) []Route {
 	routes = append(routes, weapon_melee_routes...)
 	routes = append(routes, weapon_ranged_routes...)
 
-	for _, route := range routes {
-		fmt.Printf("[%s] -> '%s' -> %s()\n", route.Method, locale+route.Endpoint, route.Handler)
-	}
-
-	fmt.Printf("Routes for locale='%s' initialized successfully!\n", locale)
-
 	return routes
 }
 
 func isValidEndpoint(locales []string, endpoint string, method string) bool {
 	for _, locale := range locales {
-		for _, route := range initRoutes(locale) {
+		for _, route := range GetRoutes() {
 			if route.Method != method {
 				continue
 			}
